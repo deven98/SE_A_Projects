8cc7b348c38d1aa6c8a1d1b3268fd0473241f2ba
mychemromance <shubhamphoenix99@outlook.com>
2017-07-14 03:50:04 -0700

    Insert added

--- Bookstore/Bookstore/Bookstore.cpp
+++ Bookstore/Bookstore/Bookstore.cpp
@@ -3,18 +3,64 @@
 
 #include "stdafx.h"
 #include<iostream>
+#include<string>
+
 using namespace std;
 //Initial commit
 
-class book
+class Book
 {
 private:
 	string name, price, pub;
 	int price;
+	Book *next;
+
+public:
+	void insert()
+	{
+		Book *newBook = new Book;
+		cout << "Enter the book name\n";
+		cin >> name;
+		cout << "Enter the price";
+		cin >> price;
+		cout << "Enter the publisher's name\n";
+		cin >> pub;
+		if (head == NULL) //create a head
+		{
+			newBook->next = NULL;
+			head = newBook;
+		}
+		else// insert at head
+		{
+			newBook->next = head;
+			head = newBook;
+		}
+	}
 };
+
+Book *head = NULL;
+
+
 
 int main()
 {
+	bool isSeller;
+	cout << "Enter 1 to buy books\nEnter 2 to sell\n";
+	int c;
+	cin >> c;
+	if (c == 1)
+		isSeller = false;
+	else
+		isSeller = true;
+
+	if (isSeller)
+	{
+
+	}
+	else
+	{
+
+	}
     return 0;
 }
 
