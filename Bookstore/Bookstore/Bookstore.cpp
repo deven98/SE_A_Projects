20bd4c07845af90a4d60066852c90ebe546bab5e
Deven <deven9852@gmail.com>
2017-07-14 15:59:41 +0530

    Class def

--- Bookstore/Bookstore/Bookstore.cpp
+++ Bookstore/Bookstore/Bookstore.cpp
@@ -1,13 +1,35 @@
-// Bookstore.cpp : Defines the entry point for the console application.
+// Bookstore .cpp : Defines the entry point for the console application.
 //
 
 #include "stdafx.h"
 #include<iostream>
 
-//Initial commit
+using namespace std;
+
+class Book {
+
+private:
+	char name[20];
+	int price;
+	char publisher[20];
+
+public:
+
+	char getName() {
+		return *name;
+	}
+
+	void setName(string nameStr) {
+		strcpy_s(name, nameStr.c_str());
+	}
+
+};
 
 int main()
 {
+	Book one ;
+	one.setName("Random");
+	cout << one.getName();
     return 0;
 }
 
