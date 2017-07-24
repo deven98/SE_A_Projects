3cb5011f67a55bcab8f99b6b5f68eaf47d1b5a56
Deven <deven9852@gmail.com>
2017-07-24 18:46:32 +0530

    Initial Commit

---
+++ MultipleInheritanceDemo/MultipleInheritanceDemo/MultipleInheritanceDemo.cpp
@@ -0,0 +1,20 @@
+/*
+Created by DEVEN on 24/7/2017
+*/
+
+// MultipleInheritanceDemo.cpp : Defines the entry point for the console application.
+// Author: Deven Joshi
+// GitHub: www.github.com/deven98
+
+#include "stdafx.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World";
+
+    return 0;
+}
+
